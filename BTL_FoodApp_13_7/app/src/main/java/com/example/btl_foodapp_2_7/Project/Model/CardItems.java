02c08d09d46d21934674da56c1c1122113cbe582
duchoaang <hoangbrato@gmail.com>
2023-08-31 19:54:29 +0700

    hoang

---
+++ BTL_FoodApp_13_7/app/src/main/java/com/example/btl_foodapp_2_7/Project/Model/CardItems.java
@@ -0,0 +1,60 @@
+package com.example.btl_foodapp_2_7.Project.Model;
+
+public class CardItems {
+    private String tenMon;
+    private String ten_User;
+    private String moTa;
+    private int luotDanhGia;
+    private int luotTim;
+
+    public CardItems(String tenMon, String ten_User, String moTa, int luotDanhGia, int luotTim) {
+        this.tenMon = tenMon;
+        this.ten_User = ten_User;
+        this.moTa = moTa;
+        this.luotDanhGia = luotDanhGia;
+        this.luotDanhGia = luotDanhGia;
+        this.luotTim = luotTim;
+    }
+
+    public String getTenMon() {
+        return tenMon;
+    }
+
+    public void setTenMon(String tenMon) {
+        this.tenMon = tenMon;
+    }
+
+    public String getTen_User() {
+        return ten_User;
+    }
+
+    public void setTen_User(String ten_User) {
+        this.ten_User = ten_User;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public int getLuotDanhGia() {
+        return luotDanhGia;
+    }
+
+    public void setLuotDanhGia(int luotDanhGia) {
+        this.luotDanhGia = luotDanhGia;
+    }
+
+    public int getLuotTim() {
+        return luotTim;
+    }
+
+    public void setLuotTim(int luotTim) {
+        this.luotTim = luotTim;
+    }
+
+
+}
