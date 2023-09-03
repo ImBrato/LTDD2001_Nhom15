b43075856dade427b68cf028de1c93eb9b18961c
huynhMinhHoang <2051052046hoang@ou.edu.vn>
2023-09-03 23:53:27 +0700

    new

--- BTL_FoodApp_13_7/app/src/main/java/com/example/btl_foodapp_2_7/Project/Activity/AdminActivity.java
+++ BTL_FoodApp_13_7/app/src/main/java/com/example/btl_foodapp_2_7/Project/Activity/AdminActivity.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.btl_foodapp_2_7.Project.Fragments.Flagments_admin_reply;
 import com.example.btl_foodapp_2_7.Project.Fragments.Fragment_admin_caidat;
 import com.example.btl_foodapp_2_7.Project.Fragments.fragment_admin_mail;
 import com.example.btl_foodapp_2_7.R;
@@ -21,18 +20,17 @@ public class AdminActivity extends AppCompatActivity {
     private LinearLayout mailLayout;
     private LinearLayout caiDatLayout;
 
-    private LinearLayout repLyLayout;
 
 
     private ImageView mailImg;
     private ImageView caiDatImg;
 
-    private ImageView repLyImg;
+
 
     private TextView mailTxt;
     private TextView caiDatTxt;
 
-    private TextView repLyTxt;
+
 
 
 
@@ -48,17 +46,15 @@ public class AdminActivity extends AppCompatActivity {
 //        code botom
         mailLayout = findViewById(R.id.mailLayout);
         caiDatLayout = findViewById(R.id.caiDatLayout);
-        repLyLayout = findViewById(R.id.repLyLayout);
+
 
 
         mailImg = findViewById(R.id.mailLayoutImg);
         caiDatImg = findViewById(R.id.caiDatLayoutImg);
-        repLyImg = findViewById(R.id.repLyLayoutImg);
 
 
         mailTxt = findViewById(R.id.mailLayoutTxt);
         caiDatTxt = findViewById(R.id.caiDatLayoutTxt);
-        repLyTxt = findViewById(R.id.repLyLayoutTxt);
 
 
 
@@ -73,13 +69,10 @@ public class AdminActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.homeFragment, fragment_admin_mail.class, null).commit();
 
                     caiDatTxt.setVisibility(View.GONE);
-                    repLyTxt.setVisibility(View.GONE);
 
                     caiDatImg.setImageResource(R.drawable.test4);
-                    repLyImg.setImageResource(R.drawable.ic_admin_reply);
 
                     caiDatLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-                    repLyLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
 
 
                     mailTxt.setVisibility(View.VISIBLE);
@@ -97,36 +90,7 @@ public class AdminActivity extends AppCompatActivity {
             }
         });
 
-        repLyLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (selecTab != 2) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.homeFragment, Flagments_admin_reply.class, null).commit();
 
-                    mailTxt.setVisibility(View.GONE);
-                    caiDatTxt.setVisibility(View.GONE);
-
-                    mailImg.setImageResource(R.drawable.ic_admin_thongbao);
-                    caiDatImg.setImageResource(R.drawable.test4);
-
-                    mailLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-                    caiDatLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-
-
-                    repLyTxt.setVisibility(View.VISIBLE);
-                    repLyImg.setImageResource(R.drawable.ic_admin_reply2);
-                    repLyLayout.setBackgroundResource(R.drawable.bg_bottom_item);
-
-                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f, 1.0f, 1f, 1f, Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f);
-                    scaleAnimation.setDuration(200);
-                    scaleAnimation.setFillAfter(true);
-                    repLyLayout.startAnimation(scaleAnimation);
-
-                    selecTab = 2;
-
-                }
-            }
-        });
 
         caiDatLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -138,13 +102,10 @@ public class AdminActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.homeFragment, Fragment_admin_caidat.class, null).commit();
 
                     mailTxt.setVisibility(View.GONE);
-                    repLyTxt.setVisibility(View.GONE);
 
                     mailImg.setImageResource(R.drawable.ic_admin_thongbao);
-                    repLyImg.setImageResource(R.drawable.ic_admin_reply);
 
                     mailLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-                    repLyLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
 
 
 
